0d95bdaa4b19a805afc5bb6316966b77208d2d4f
gianpy15 <gianpaolo.dipietro@mail.polimi.it>
2018-05-22 19:59:14 +0200

    Initial commit + readme

---
+++ README.md
@@ -0,0 +1,1 @@
+## Aca2018 project
---
+++ source/tests/test_mkl_dnn.cpp
@@ -0,0 +1,11 @@
+//
+// Created by gianpaolo on 5/22/18.
+//
+
+#include <iostream>
+#include "mkldnn.hpp"
+
+int main(){
+    std::cout << "This is a test" << std::endl;
+    return 0;
+}
