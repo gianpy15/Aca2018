75b6ade6ed3683d45119c2826516746e4baeae85
Luca <cavalli1234@hotmail.it>
2018-05-25 11:17:10 +0200

    merged test codes

--- source/main.cpp
+++ source/main.cpp
@@ -9,35 +9,34 @@ const int kernel_11x11[] = {11, 11};
 const int no_stride[] = {1, 1};
 const int four_stride[] = {4, 4};
 
-AbsNet * test_fpNet(int repetitions) {
-    auto *net = new FPNetwork({1, 3, 227, 227});
-    std::cout << "Network initialized" << std::endl;
-    net->addConv2D(64, kernel_11x11, four_stride, Padding::VALID);
-    std::cout << "Added first convolution" << std::endl;
-    net->addConv2D(64, kernel_3x3, no_stride, Padding::VALID);
-    std::cout << "Added second convolution" << std::endl;
-    net->setup_net();
-    std::cout << "Network setup successful!" << std::endl;
-    net->run_net(repetitions);
 
+AbsNet * setup_test_net(AbsNet* baseNet){
+    std::cout << "Network initialized" << std::endl;
+    baseNet->addConv2D(64, kernel_11x11, no_stride, Padding::VALID);
+    std::cout << "Added first convolution" << std::endl;
+    baseNet->addConv2D(64, kernel_3x3, no_stride, Padding::VALID);
+    std::cout << "Added second convolution" << std::endl;
+    baseNet->setup_net();
+    std::cout << "Network setup successful!" << std::endl;
+    return baseNet;
+}
+
+AbsNet * test_fpNet(int repetitions) {
+    AbsNet *net = new FPNetwork({1, 3, 227, 227});
+    net = setup_test_net(net);
+    net->run_net(repetitions);
     return net;
 }
 
 AbsNet * test_intNet(int repetitions) {
     AbsNet *net = new INTNetwork({1, 3, 227, 227});
-    std::cout << "Network initialized" << std::endl;
-    net->addConv2D(64, kernel_11x11, no_stride, Padding::VALID);
-    std::cout << "Added first convolution" << std::endl;
-    net->addConv2D(64, kernel_3x3, no_stride, Padding::VALID);
-    std::cout << "Added second convolution" << std::endl;
-    net->setup_net();
-    std::cout << "Network setup successful!" << std::endl;
+    net = setup_test_net(net);
     net->run_net(repetitions);
-
     return net;
 }
 
+
 int main() {
-    //AbsNet *net = test_fpNet(1);
+    AbsNet *net = test_fpNet(1);
     AbsNet *net2 = test_intNet(1);
 }
