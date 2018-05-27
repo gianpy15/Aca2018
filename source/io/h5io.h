756ab2dddd71f5126d0f0ac2bb279a6e2a19bba6
gianpy15 <gianpaolo.dipietro@mail.polimi.it>
2018-05-27 17:43:53 +0200

    Add reader API script for building the library

---
+++ source/io/h5io.h
@@ -0,0 +1,14 @@
+//
+// Created by gianpaolo on 5/26/18.
+//
+
+#ifndef ACA2018_H5IO_H
+#define ACA2018_H5IO_H
+
+#endif //ACA2018_H5IO_H
+
+#include <iostream>
+#include <string>
+#include "H5Cpp.h"
+
+int main();
